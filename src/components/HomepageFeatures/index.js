e32bdeca7b425f4cad127f5c73cd7ffe5d749b04
amirayuyue <amirayuyue@gmail.com>
2026-05-19 17:09:46 -0700

    edited the css

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,46 +1,41 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-import link from '@docusaurus/Link';
+
 const FeatureList = [
   {
-    title: 'The First Spot for HPC Documentation @ UCM',
+    title: 'HPC Documentation',
     Svg: require('@site/static/img/liftup.svg').default,
     description: (
       <>
-       This site is tailored and designed for the UC Merced community that use the HPC clusters.
-       All job scripts and software tutorials are done on the cluster for best experience possible. 
+        Guides, job scripts, and software tutorials built for the UC Merced HPC clusters.
       </>
     ),
     link: 'docs/category/HPC-clusters',
   },
   {
-    title: 'Centralized Hub for Both Jupyterhub and HPC Clusters',
+    title: 'JupyterHub Documentation',
     Svg: require('@site/static/img/globe.svg').default,
     description: (
       <>
-        First place to visit for everything HPC and JupyterHub related at UC Merced.
-        Constantly updated with the latest news, information and resources.
+        Your go-to resource for JupyterHub and HPC at UC Merced, kept up to date.
       </>
     ),
-    link: 'docs/jupyter/jupyterhub-policy', 
+    link: 'docs/jupyter/jupyterhub-policy',
   },
   {
-    title: 'An Open-Source Collaborative Effort',
+    title: 'Join the Community',
     Svg: require('@site/static/img/interconnect.svg').default,
     description: (
       <>
-       We are a tight knit group of researchers who are passionate about HPC and JupyterHub.
-       We are always looking for new contributors to help us improve our documentation.
-       Join us and chat with us on our{" "}
-       <a href="https://ucmhpcclusters.slack.com/signup#/domain-signup" target="_blank" rel="noopener noreferrer">
-             Slack workspace 
-       </a> {" "}
-
-        or  open an issue on our {" "}
-       <a href="https://github.com/ucm-it/hpc_docs/issues" target="_blank" rel="noopener noreferrer">
-         GitHub page
-       </a>.
+        Chat with us on{" "}
+        <a href="https://ucmhpcclusters.slack.com/signup#/domain-signup" target="_blank" rel="noopener noreferrer">
+          Slack
+        </a>{" "}
+        or contribute on{" "}
+        <a href="https://github.com/ucm-it/hpc_docs/issues" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>.
       </>
     ),
     link: 'https://ucmhpcclusters.slack.com/signup#/domain-signup',
@@ -56,7 +51,9 @@ function Feature({Svg, title, description, link}) {
         </a>
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <a href={link} className={styles.featureTitleLink}>
+          <Heading as="h3" className={styles.featureTitle}>{title}</Heading>
+        </a>
         <p>{description}</p>
       </div>
     </div>
